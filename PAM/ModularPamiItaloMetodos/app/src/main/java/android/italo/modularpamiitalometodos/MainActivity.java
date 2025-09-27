026b433a0050c7b2f0589d969869c5db7f34e417
ITALO_5119 <ITALO_5119@uirapuru.br>
2025-09-26 21:46:31 -0300

    android

---
+++ PAM/ModularPamiItaloMetodos/app/src/main/java/android/italo/modularpamiitalometodos/MainActivity.java
@@ -0,0 +1,41 @@
+package android.italo.modularpamiitalometodos;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText nome,email,senha;
+    
+    Button btnCadastrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initComponent();
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void initComponent() {
+        nome = findViewById(R.id.edt_nome);
+        email = findViewById(R.id.edt_email);
+        senha = findViewById(R.id.edt_senha);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+    }
+}
